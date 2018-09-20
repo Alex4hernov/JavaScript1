afe8f021b978e2a56d6481f4388f5bb3a73f6071
Alexchernov <Alexchernov12@gmail.com>
2018-09-20 14:56:06 +0300

    ДЗ №5

--- js/main.js
+++ js/main.js
@@ -1,60 +1,44 @@
 "use strict";
 
-// Задание №1
-var objNumber = {};
-function NumToObj(){
-    var arrNumber = inputNumber.split('');
-    while(arrNumber.length < 3){
-            arrNumber.unshift(0);
-        }
-    if (arrNumber.length > 3){
-        return null;
-    }
-    else {
-        objNumber['сотни'] = +arrNumber[0];
-        objNumber['десятки'] = +arrNumber[1];
-        objNumber['единицы'] = +arrNumber[2];
-        return objNumber;
-    }
-}
-var inputNumber = prompt('Ведите число от 0 до 999');
-if (NumToObj(inputNumber) == null){
-console.log('Введено неверное кол-во символов');   
-}
-else {
-    console.log(objNumber);
-}
 
-
-// Задание №2
-var products = [{
-    id: 1,
-    title: 'item 1',
-    price: 10,
-    },{
-    id: 2,
-    title: 'item 2',
-    price: 20,
-    },{
-    id: 3,
-    title: 'item 3',
-    price: 30,
-    },
-];
-var cart = {
-    products: [],
-    sum: 0,
-    setProducts(products){
-        this.products = products;
-        this.countTotalPrice();
-    },
-    countTotalPrice(){
-        var sum = 0;
-        for(var i = 0; i < this.products.length; i++) {
-            sum += this.products[i].price;
+function chessboard() {
+    var table = document.createElement( 'table' ),
+        lets = [ '','A','B','C','D','E','F','G','H','' ],
+        blackFigs1 = [ '8','&#9820;','&#9822;','&#9821;','&#9819;','&#9818;','&#9821;','&#9822;','&#9820;','8' ],
+        whiteFigs1 = [ '1','&#9814;','&#9816;','&#9815;','&#9813;','&#9812;','&#9821;','&#9815;','&#9814;','1' ],
+        blackFigs2 = [ '7','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','&#9823;','7' ],
+        whiteFigs2 = [ '2','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','&#9817;','2' ];
+    for ( var i = 0, a = 9; i < 10, a >= 0; i++, a-- ) {
+        var tr = table.insertRow(i);
+        for ( var j = 0; j < 10; j++ ) {
+            var td = tr.insertCell( j );
+                        switch (i) {
+                case 0:
+                    td.innerText = lets[ j ];
+                    break;
+                case 1:
+                    td.innerHTML = blackFigs1[ j ];
+                    break;
+                case 2:
+                    td.innerHTML = blackFigs2[ j ];
+                    break;
+                case 7:
+                    td.innerHTML = whiteFigs2[ j ];
+                    break;
+                case 8:
+                    td.innerHTML = whiteFigs1[ j ];
+                    break;
+                case 9:
+                    td.innerText = lets[ j ];
+                    break;
+                default:
+                    if ( j === 0 || j === 9 ) {
+                        td.innerHTML = a;
+                    }
+                    break;
+            }
         }
-        this.sum = sum;
     }
+    document.body.appendChild( table );
 };
-console.log(cart);
-cart.setProducts(products);
+chessboard();
